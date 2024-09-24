ea0213414779238a3330dd01b835576f06230751
SuperStar09030826 <superstar09030826@gmail.com>
2024-09-24 20:55:41 +1000

    content update

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,7 +9,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 // MAIN FUNCTION
 export default function Main() {
   return (
-    <div id='home' className='w-full h-screen text-center'>
+    <div id='home' className='w-full h-screen text-center bg-[url("/assets/contact.jpg")] bg-cover'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-800'>
@@ -22,7 +22,7 @@ export default function Main() {
           <h2 className='py-2 text-gray-800'>
             Your web3 trusted partner
           </h2>
-          <p className='py-4 text-gray-800 sm:max-w-[70%] m-auto'>
+          <p className='py-4 text-gray-800 sm:max-w-[70%] m-auto font-medium'>
             At Blockchain & Star, we specialize in creating and
             developing custom blockchain solutions, offering expert
             consulting, and providing training in web3 and blockchain
